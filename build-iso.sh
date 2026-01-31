9ba74379e6599597cb663271ae492a438fbe8d56
Saki Vali <152751956+SakiVali@users.noreply.github.com>
2026-01-30 21:07:48 -0700

    changed to only rm self if build success

--- build-iso.sh
+++ build-iso.sh
@@ -2,5 +2,4 @@ scriptloc=$(dirname $0)
 cp -r /usr/share/archiso/configs/releng/ $scriptloc
 cp $scriptloc/ais.sh $scriptloc/releng/airootfs/install
 chmod a+x $scriptloc/releng/airootfs/install
-mkarchiso -v -r -w /tmp/archiso-tmp -o $scriptloc/../ $scriptloc/releng
-rm -rf $scriptloc
+mkarchiso -v -r -w /tmp/archiso-tmp -o $scriptloc/../ $scriptloc/releng && rm -rf $scriptloc
