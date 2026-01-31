775147d21c6a59e90912e430411d702e3d43ed57
Saki Vali <152751956+SakiVali@users.noreply.github.com>
2026-01-30 19:36:21 -0700

    added self removal

--- build-iso.sh
+++ build-iso.sh
@@ -1,4 +1,5 @@
 scriptloc=$(dirname $0)
- cp -r /usr/share/archiso/configs/releng/ $scriptloc
+cp -r /usr/share/archiso/configs/releng/ $scriptloc
 cp $scriptloc/ais.sh $scriptloc/releng/airootfs/install
-mkarchiso -v -r -w /tmp/archiso-tmp -o $scriptloc/ $scriptloc/releng
+mkarchiso -v -r -w /tmp/archiso-tmp -o $scriptloc/../ $scriptloc/releng
+rm -rf scriptloc
