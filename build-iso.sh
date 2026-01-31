13f87b4d738b901107dabae8400016b5d4b781fc
Saki Vali <152751956+SakiVali@users.noreply.github.com>
2026-01-30 19:34:57 -0700

    updated for self building.

--- build-iso.sh
+++ build-iso.sh
@@ -1,3 +1,4 @@
 scriptloc=$(dirname $0)
-cp $scriptloc/ais.sh $scriptloc/arch-iso/releng/airootfs/install
-mkarchiso -v -r -w /tmp/archiso-tmp -o $scriptloc/ $scriptloc/arch-iso/releng
+ cp -r /usr/share/archiso/configs/releng/ $scriptloc
+cp $scriptloc/ais.sh $scriptloc/releng/airootfs/install
+mkarchiso -v -r -w /tmp/archiso-tmp -o $scriptloc/ $scriptloc/releng
