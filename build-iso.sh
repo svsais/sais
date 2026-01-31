d18d50e8ff5df431dd3430ec30118bc3c10aa423
Saki Vali <152751956+SakiVali@users.noreply.github.com>
2026-01-30 22:41:03 -0700

    added perm updating for script

--- build-iso.sh
+++ build-iso.sh
@@ -1,4 +1,7 @@
 scriptloc=$(dirname $0)
 cp -r /usr/share/archiso/configs/releng/ $scriptloc
 cp $scriptloc/ais.sh $scriptloc/releng/airootfs/install
+sed -i '$ d' $scriptloc/releng/profiledef.sh
+echo '["/install"]="0:0:777"' >> $scriptloc/releng/profiledef.sh
+echo ')' >> $scriptloc/releng/profiledef.sh
 mkarchiso -v -r -w /tmp/archiso-tmp -o $scriptloc/../ $scriptloc/releng && rm -rf $scriptloc
